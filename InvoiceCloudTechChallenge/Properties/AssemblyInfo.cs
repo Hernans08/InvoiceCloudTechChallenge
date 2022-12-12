bcc948667fdc8e9c105ef79e002f8cd34aeb4ea1
Hernans08 <hsolorzano79@gmail.com>
2022-12-12 01:20:33 -0600

    Add project files.

---
+++ InvoiceCloudTechChallenge/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("InvoiceCloudTechChallenge")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("InvoiceCloudTechChallenge")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5a76ad11-6ddc-4b60-b159-bab5ba9c8307")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InvoiceCloudTechChallenge/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace InvoiceCloudTechChallenge
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/add_remove_elements/");
+
+            for (int i = 0; i < 5; i++)
+            {
+                driver.FindElement(By.XPath("//button[contains(text(), 'Add Element')]")).Click();
+            }
+
+            try
+            {
+                List<IWebElement> e = new List<IWebElement>();
+                e.AddRange(driver.FindElements(By.XPath("//button[contains(text(), 'Delete')]")));
+                Assert.IsTrue(driver.FindElement(By.XPath("//button[contains(text(),'Delete')]")).Displayed);
+                Debug.WriteLine("Assertion Passed. A total of " + e.Count + " elements were found.");
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
